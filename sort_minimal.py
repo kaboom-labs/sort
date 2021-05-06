59425466ee3541f745e60dcfaaf4ac5d007dcf20
Jason <tensorturtle@gmail.com>
2021-05-06 21:51:10 +0900

    remove more dependencies

--- sort_minimal.py
+++ sort_minimal.py
@@ -33,8 +33,8 @@ import numpy as np
 #import matplotlib.patches as patches
 #from skimage import io
 
-import glob
-import time
+#import glob
+#import time
 #import argparse
 from filterpy.kalman import KalmanFilter
 
